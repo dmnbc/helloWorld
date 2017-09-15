ada315385b1911b0372447c9dcb3441f98804ad3
dmnbc <dmnbc@detlef-kremer.com>
2017-09-15 16:49:38 +0200

    /tr fehlte

--- index.php
+++ index.php
@@ -1,35 +1,54 @@
 <html>
-	<head>
+
+<head>
 	<meta content="utf-8">
 	<title>Beispieldateien zu HTML</title>
 	<meta name="Keywords" content="Liste">
 	<meta name="Description" content="Liste der Dateien mit Schlüsselworten">
-		
-	</head>
-	<body>
+
+</head>
+
+<body>
 	<h3>Herzlich Willkommen auf den Beispielseiten zum HTML Kurs.</h3>
 	<h4><a href="http://fachwissen.click/course/view.php?id=55">Den Kurs finden Sie hier</a></h4>
-	
+
 	<h5>Die Dateien im Einzelnen</h5>
-	
+
 	<table>
-		<tr><th>Bezeichnung</th><th>Begriffe</th><th>Thema</th></tr>
-		
-		<tr><td>0001.html</td><td>einfacher Textfluss</td><td></td></tr>
-		<tr><td>0002.html</td><td>Umbruch, br, </td><td></td>/tr>
-		
-		
-		<!-- todo  PHP script scandir get_meta_tags 
+		<tr>
+			<th>Bezeichnung</th>
+			<th>Begriffe</th>
+			<th>Thema</th>
+		</tr>
+
+		<tr>
+			<td>0001.html</td>
+			<td>einfacher Textfluss</td>
+			<td></td>
+		</tr>
+		<tr>
+			<td>0002.html</td>
+			<td>Umbruch, br, </td>
+			<td></td>
+		</tr>
+
+
+			<!-- todo  PHP script scandir get_meta_tags 
 		        Link setzen      <meta name="keywords" <br>
                                    content="einfacher Textfluss">     -->
-         
-        <!-- $tags = get_meta_tags("metaTags.html"); -->
-<tr>
-	<td>metaTags.html</td>
-	<td><?php echo get_meta_tags("metaTags.html")['keywords'];   ?></td>
-	<td><?php echo get_meta_tags("metaTags.html")["DESCRIPTION"];?></td>
-</tr>
+
+			<!-- $tags = get_meta_tags("metaTags.html"); -->
+			<tr>
+				<td>metaTags.html</td>
+				<td>
+					<?php echo get_meta_tags("metaTags.html")['keywords'];   ?>
+				</td>
+				<td>
+					<?php echo get_meta_tags("metaTags.html")["DESCRIPTION"];?>
+				</td>
+			</tr>
 	</table>
-		
-	</body>
+
+</body>
+
 </html>
