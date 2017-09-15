191adf715221e6ece69d1f037e43e7b3e51904a2
dmnbc <dmnbc@detlef-kremer.com>
2017-09-15 16:44:38 +0200

    description

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 	<head>
 	<meta content="utf-8">
 	<title>Beispieldateien zu HTML</title>
+	<meta name="Keywords" content="Liste">
+	<meta name="Description" content="Liste der Dateien mit Schlüsselworten">
 		
 	</head>
 	<body>
@@ -9,6 +11,7 @@
 	<h4><a href="http://fachwissen.click/course/view.php?id=55">Den Kurs finden Sie hier</a></h4>
 	
 	<h5>Die Dateien im Einzelnen</h5>
+	
 	<table>
 		<tr><th>Bezeichnung</th><th>Begriffe</th><th>Thema</th></tr>
 		
@@ -24,7 +27,7 @@
 <tr>
 	<td>metaTags.html</td>
 	<td><?php echo get_meta_tags("metaTags.html")['keywords'];   ?></td>
-	<td><?php echo get_meta_tags("metaTags.html")['DESCRIPTION'];?></td>
+	<td><?php echo get_meta_tags("metaTags.html")["DESCRIPTION"];?></td>
 </tr>
 	</table>
 		
