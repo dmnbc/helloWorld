60e48ac492880a314bc136ec9a1a19443aca3c11
dmnbc <dmnbc@detlef-kremer.com>
2017-09-17 09:07:19 +0200

    beispielb zu p tag

---
+++ Absatz.php
@@ -0,0 +1,17 @@
+<!doctype html>    
+<html>
+<head>
+<meta content="utf-8">
+
+<title>Paragraph</title>
+	<meta name="Keywords" content="p">
+	<meta name="Description" content="Ansatzbildung mit Paragraphenelementen">
+
+</head>
+
+<body>
+Zeile ohne p <p>Diese Zeile beginnt und endet mit einem Paragraphen.</p>Die nächste Zeile ist wieder ohne p.<p>Eine erste Ziele mit p und /p </p><p>Eine zweite Ziele mit p und /p </p>
+
+
+	</body>
+</html>
